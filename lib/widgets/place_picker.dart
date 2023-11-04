cb5f1a523d3a19815ce128de225b8c3f058158b9
jumpingpixelpt <70814520+jumpingpixelpt@users.noreply.github.com>
2023-11-04 09:07:22 +0000

    Update place_picker.dart

--- lib/widgets/place_picker.dart
+++ lib/widgets/place_picker.dart
@@ -146,7 +146,7 @@ class PlacePickerState extends State<PlacePicker> {
           key: this.appBarKey,
           title: SearchInput(searchPlace),
           centerTitle: true,
-          automaticallyImplyLeading: false,
+          automaticallyImplyLeading: true,
         ),
         body: Column(
           children: <Widget>[
